5608ec3875ef028dfb441ebce3f7c9d8abd6ec7c
SDK <sadiksaifi205@gmail.com>
2022-08-13 02:00:49 +0530

    misc

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-paru -S bspwm polybar sxhkd montserrat-otf ttf-nerd-fonts-hack-complete-git udiskie udisks2 alacritty dunst feh picom-ibhagwan-git xorg-xsetroot xorg-xbacklight xbanish pamixer neofetch tty-clock lf-bin htop exa gpick nitrogen flameshot tdrop
+paru -S bspwm polybar sxhkd montserrat-otf ttf-nerd-fonts-hack-complete-git udiskie udisks2 alacritty dunst feh picom-ibhagwan-git xorg-xsetroot xorg-xbacklight xbanish pamixer neofetch tty-clock lf-bin htop exa gpick nitrogen flameshot tdrop tty-clock
